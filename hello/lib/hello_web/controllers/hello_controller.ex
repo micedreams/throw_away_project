f400d5c9ff05bc23503181e4e9e01b34d5227744
Akshatha <micedreams@Aku-Computer.domain.name>
2020-06-28 19:48:48 +0530

    Using Routes and Params

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,6 +1,6 @@
 defmodule HelloWeb.HelloController do
     use HelloWeb, :controller
-    def world(conn, _params) do
-        render(conn, "world.html")
+    def world(conn, %{"name" => name}) do
+        render(conn, "world.html",name: name)
     end
 end
